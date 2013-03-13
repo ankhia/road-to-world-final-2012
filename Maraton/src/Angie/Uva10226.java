1585febd9327adfba37abb961b2ef37bafaf56f7
Milena <Milena@MilenaV>
2013-03-12 21:14:44 -0500

    cambio

--- Maraton/src/Angie/Uva10226.java
+++ Maraton/src/Angie/Uva10226.java
@@ -1,29 +1,12 @@
 package Angie;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.TreeMap;
-import java.util.TreeSet;
 
 public class Uva10226 {
 
-	static TreeMap<char[], Integer> h = new TreeMap<char[], Integer>(new Comparator<char[]>() {
-		public int compare(char[] o1, char[] o2) {
-			int min = o1.length < o2.length ? o1.length : o2.length;
-			for (int i = 0; i < min; i++) {
-				if(o1[i]!=o2[i])
-					return o1[i]-o2[i];
-			}
-			if(o1.length > o2.length)
-				return 1;
-			else if(o2.length > o1.length)
-				return -1;
-			return 0;
-		}
-	});
-	static ArrayList<String> a = new ArrayList<String>();
-	static TreeSet<String> b= new TreeSet<String>();
+	static TreeMap<String, Integer> h = new TreeMap<String, Integer>();
 	public static void main(String[] args) throws Throwable {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt( in.readLine( ) );
@@ -31,18 +14,15 @@ public class Uva10226 {
 		for (int i = 0; i < t; i++) {
 			int total = 0;
 			for( String line; (line = in.readLine())!=null && !line.equals("");  ) {
-				char[] v = line.toCharArray( );
-//				a.add(line);
-//				b.add(line);
-				if( h.get(v) == null )
-					h.put(v, 1);
+				if( h.get(line) == null )
+					h.put(line, 1);
 				else{
-					h.put(v, h.get(v)+1);
+					h.put(line, h.get(line)+1);
 				}
 				total ++;
 			}
-			for( char[] x : h.keySet() ) {
-				System.out.printf(new String(x)+" %.4f\n", ( total > 0 ? ( (double)(h.get(x)*100 ) / total  ): 0.  ) );
+			for( String x : h.keySet() ) {
+				System.out.printf(x+" %.4f\n", ( total > 0 ? ( (double)(h.get(x)*100 ) / total  ): 0.  ) );
 			}
 		}
 	}
