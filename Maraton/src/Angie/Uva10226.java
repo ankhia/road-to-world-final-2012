0cc7503548c3102976d453f973eb1a8e2c46c812
Angie Milena Vega Leon <angie.vega@helisa.com>
2013-03-13 10:50:36 -0500

    Coments old version

--- Maraton/src/Angie/Uva10226.java
+++ Maraton/src/Angie/Uva10226.java
@@ -5,7 +5,6 @@ import java.io.InputStreamReader;
 import java.text.NumberFormat;
 import java.util.Locale;
 import java.util.TreeMap;
-
 public class Uva10226 {
 
 	static TreeMap<String, Integer> h = new TreeMap<String, Integer>();
@@ -27,18 +26,18 @@ public class Uva10226 {
 				}
 				total++;
 			}
-			
 			for( String x : h.keySet() ) {
-				sb.append(x+" "+nf.format(100.*h.get(x)/total)+"\n");
+				//THIS WAS AN ERROR
+				System.out.printf(Locale.US, "%s %.4f\n", x, 100.*h.get(x)/total ); 
+//				sb.append(x+" "+nf.format(100.*h.get(x)/total)+"\n");
 			}
-			if( i < t-1 )
-				sb.append("\n");
+			if( i < t-1 ){
+				System.out.println();
+//				sb.append("\n");
+			}
+			//THIS WAS AN ERROR
 			h.clear();
-			if(sb.length()>1000000){
-				System.out.print(new String(sb));
-				sb.delete(0, sb.length());
-			}
 		}
-		System.out.print(new String(sb));
+//		System.out.print(new String(sb));
 	}
 }
